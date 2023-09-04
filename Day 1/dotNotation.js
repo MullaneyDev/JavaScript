50f43132871e0fd69e9a9b197d748eb13b5c0408
MullaneyDev <matt.mullaney@live.co.uk>
2023-09-04 14:30:59 +0100

    Update of notes

--- Day 1/dotNotation.js
+++ Day 1/dotNotation.js
@@ -1,51 +1,70 @@
-console.log ("Hello world")
+console.log ("Hello world");
 // first line of code. displays Hello world in the terminal
 
 // cmd + / will comment out code and not run
 
-console.log ("This is a string!")
+console.log ("This is a string!");
 //strings are used to show chracters - they are surrounded by speech marks
 
-console.log ("1234")
+console.log ("1234");
 // This is still a string as it is in speech marks
 
-console.log (1234)
+console.log (1234);
 //This data type is number
 
-console.log (12.34)
+console.log (12.34);
 //This data type is still a number
 
-console.log (true)
-console.log (false)
+console.log (true);
+console.log (false);
 //These are booleans - true or false, nothing inbetween
 
-console.log ("This is a string".length)
+console.log ("This is a string".length);
 //Gives the length of the string in characters - displayed in the terminal
 //Doesn't work for other data types (e.g numbers - doesn't have a length, it has a value. boolean - is either true or false)
 
-console.log ("hello".toUpperCase())
+console.log ("hello".toUpperCase());
 //changes the string "hello" to uppercase letters
 
-console.log ("HELLO".toLowerCase())
+console.log ("HELLO".toLowerCase());
 //changes the string "HELLO" to lowercase letters
 
-console.log (Math.random())
+console.log (Math.random());
 //gives a randomly generated number between 0 and 1
 
-console.log (Math.random()*10)
+console.log (Math.random()*10);
 //gives a randomly generated number between 0 and 1 then multiplies it by 10
 
-console.log (Math.round(Math.random()*10))
+console.log (Math.round(Math.random()*10));
 //gives a randomly generated number between 0 and 1, multiplies it by 10 and then rounds it to a whole number
 
-console.log (Math.floor(Math.random()*10))
+console.log (Math.floor(Math.random()*10));
 //gives a randomly generated number between 0 and 1, multiplies it by 10 and will always round down regardless of its value
 
-console.log (Math.ceil(Math.random()*10))
+console.log (Math.ceil(Math.random()*10));
 //gives a randomly generated number between 0 and 1, multiplies it by 10 and will always round up regardless of its value
 
-console.log ("   |   |   ")
-console.log ("-----------")
-console.log ("   |   |   ")
-console.log ("-----------")
-console.log ("   |   |   ")
+console.log ("   |   |   ");
+console.log ("-----------");
+console.log ("   |   |   ");
+console.log ("-----------");
+console.log ("   |   |   ");
+//prints grid to console
+
+
+
+const grid = ["   |   |   ","-----------","   |   |   ","-----------","   |   |   "];
+
+for (let i = 0; i < grid.length; i++) {
+    console.log (grid[i]);
+}
+//prints grid to console using for loop.
+
+const column = "   |   |   ";
+const row = "-----------";
+
+for (let c = 0, r = 0; c < 3, r < 2; c++, r++) {
+    console.log (column);
+    console.log (row);
+}
+
---
+++ Day 1/variables.js
@@ -0,0 +1,22 @@
+console.log ("All Around the World".charAt(7).toUpperCase());
+//displays character 7 (u) in upper case
+//javascript always starts from 0
+
+let myName = "Matt";
+//assigns the name "Matt"(string) to the variable "myName"
+
+let myAge = 33;
+//assigns the value of 33(number) to the variable "myAge"
+
+const bornIn = "Chester";
+//assigns the value "Chester" to the variable "bornIn" - this is a fixed variable and cannot by changed
+
+console.log (myName);
+//displays the variable "myName" in the terminal - Matt
+
+myName = "Matthew";
+//resets the variable of "myName"
+
+console.log (myName);
+//displays the new value of "myName" in the terminal - Matthew
+
