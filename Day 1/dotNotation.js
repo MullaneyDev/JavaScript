df94eb82e656ff67c2e74b573fc79279ca3eb3af
MullaneyDev <matt.mullaney@live.co.uk>
2023-09-04 10:51:24 +0100

    Morning notes before first break

--- Day 1/dotNotation.js
+++ Day 1/dotNotation.js
@@ -1,1 +1,21 @@
 console.log("Hello world")
+// first line of code. displays Hello world in the terminal
+
+// cmd/ will comment out code and not run
+
+console.log("This is a string!")
+//strings are used to show chracters - they are surrounded by speech marks
+
+console.log("1234")
+// This is still a string as it is in speech marks
+
+console.log (1234)
+//This data type is number
+
+console.log (12.34)
+//This data type is still a number
+
+console.log (true)
+console.log (false)
+//These are booleans - true or false, nothing inbetween
+
